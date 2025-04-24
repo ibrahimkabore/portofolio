83e6583bb9e89d242b48e51eedeb993159153a32
kaboreibrahim <ibrahimkabore025@gmail.com>
2025-04-24 09:36:52 +0000

    add allowed_hosts

--- .history/porto_folio/settings_20250424093540.py
+++ .history/porto_folio/settings_20250424093540.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mon-portfolio-dh55.onrender.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
